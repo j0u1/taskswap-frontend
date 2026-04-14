87eedd85f012368dc6a527f4dc6bfbaa47399f94
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-14 19:49:39 +0300

    style(sidebar): change duration ms

--- app/components/UI/Sidebar/index.vue
+++ app/components/UI/Sidebar/index.vue
@@ -13,7 +13,7 @@ console.log(route.path);
 </script>
 
 <template>
-    <aside class="h-screen flex flex-col gap-4 w-2/12 duration-300 transition-all" :class="{ 'w-21': isCollapsed }">
+    <aside class="h-screen flex flex-col gap-4 w-2/12 duration-200 transition-all" :class="{ 'w-21': isCollapsed }">
         <div class="flex justify-between items-center pt-4" :class="[isCollapsed ? 'pl-4 pr-2 pb-1' : 'px-4']">
             <IconsLogosFull v-if="!isCollapsed" />
             <IconsLogosMini class="size-6 max-h-6 shrink-0 mt-0.75" v-else />
@@ -34,10 +34,10 @@ console.log(route.path);
                 v-for="page in pages"
                 :key="page.title"
                 :to="page.path"
-                class="flex items-center gap-2 text-gray outline outline-additional rounded-lg py-2 px-2.5 font-medium duration-300 transition-all whitespace-nowrap relative"
+                class="flex items-center gap-2 text-gray outline outline-additional rounded-lg py-2 px-2.5 font-medium duration-200 transition-all whitespace-nowrap relative"
                 :class="[
                     route.path === page.path ? 'bg-additional text-primary cursor-default' : 'hover:outline-primary',
-                    isCollapsed ? 'w-full' : '',
+                    isCollapsed ? 'size-12.75' : '',
                 ]"
             >
                 <component
@@ -46,7 +46,7 @@ console.log(route.path);
                     :class="[isCollapsed ? 'size-5 mx-auto my-2' : 'size-4.5']"
                 />
                 <span
-                    class="duration-300 transition-all origin-left"
+                    class="duration-200 transition-all origin-left"
                     :class="[isCollapsed ? 'scale-0 absolute opacity-0' : 'relative scale-100']"
                     >{{ page.title }}</span
                 >
