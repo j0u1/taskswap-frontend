85968a15d788ab66ce433a7822fd7d91e73c7d97
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-15 20:47:00 +0300

    feat(dashboard): add category statistics with normalized progress bars

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -3,6 +3,17 @@ definePageMeta({
     layout: "sidebar",
     pageTitle: "Обзор",
 });
+
+const statisticsOfCategories = [
+    { name: "Оборудование", value: 2 },
+    { name: "Сеть", value: 1 },
+    { name: "ПО", value: 3 },
+];
+const maxValue = Math.max(...statisticsOfCategories.map((c) => c.value));
+const getPercent = (value: number) => {
+    if (!maxValue) return 0;
+    return (value / maxValue) * 100 + "%";
+};
 </script>
 
 <template>
@@ -18,6 +29,18 @@ definePageMeta({
                 </div>
             </div>
         </UICard>
-        <UICard> </UICard>
+        <UICard title="Статистика по категориям">
+            <div class="flex flex-col gap-4">
+                <div v-for="category in statisticsOfCategories" class="text-sm flex justify-between p-4 border border-border rounded-lg">
+                    <div class="flex items-center justify-items-center gap-4 w-full">
+                        <span class="uppercase text-gray w-38 truncate">{{ category.name }}</span>
+                        <div class="w-full rounded-full h-2 bg-secondary overflow-hidden">
+                            <div class="h-full bg-gray rounded-full" :style="{ width: getPercent(category.value) }" />
+                        </div>
+                        <span>{{ category.value }}</span>
+                    </div>
+                </div>
+            </div>
+        </UICard>
     </div>
 </template>
