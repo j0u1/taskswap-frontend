2a0c4dad0f42f555de9ba9e7063680d26079c8a6
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-15 16:54:59 +0300

    style(sidebar): move collapsed button on the line

--- app/components/UI/Sidebar/index.vue
+++ app/components/UI/Sidebar/index.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
-import { LayoutIcon, TicketIcon, Users2Icon, PanelLeftCloseIcon, PanelLeftOpenIcon } from "@lucide/vue";
+import { LayoutIcon, Users2Icon, PanelLeftCloseIcon, PanelLeftOpenIcon, TriangleAlertIcon } from "@lucide/vue";
 
 const route = useRoute();
 const isCollapsed = ref(false);
 const pages = [
     { title: "Панель управления", path: "/dashboard", icon: LayoutIcon },
-    { title: "Тикеты", path: "/tickets", icon: TicketIcon },
+    { title: "Проблемы", path: "/tickets", icon: TriangleAlertIcon },
     { title: "Сотрудники", path: "/employees", icon: Users2Icon },
 ];
 
@@ -13,33 +13,33 @@ console.log(route.path);
 </script>
 
 <template>
-    <aside class="h-screen flex flex-col gap-4 w-2/12 duration-200 transition-all" :class="{ 'w-21': isCollapsed }">
-        <div class="flex justify-between items-center pt-4" :class="[isCollapsed ? 'pl-4 pr-2 pb-1' : 'px-4']">
-            <NuxtLink class="duration-300 transition-all active:scale-105" to="/">
+    <aside class="h-screen flex flex-col gap-4 duration-200 transition-all" :class="[ isCollapsed ? 'w-19.75' : 'w-2/12 min-w-56' ]">
+        <div class="relative flex justify-between items-center pt-6 pb-3.5 duration-300 transition-all" :class="[isCollapsed ? 'px-4 pb-1' : 'px-4']">
+            <NuxtLink class="duration-300 transition-all active:scale-105" :class="[isCollapsed ? 'mx-auto' : 'mx-0']" to="/">
                 <IconsLogosFull v-if="!isCollapsed" />
-                <IconsLogosMini class="size-6 max-h-6 shrink-0 mt-0.75" v-else />
+                <IconsLogosMini class="size-6 max-h-6 shrink-0" v-else />
             </NuxtLink>
-            <button @click="isCollapsed = !isCollapsed" class="mt-1 text-gray hover:scale-102 cursor-pointer shrink-0 active:text-primary">
+            <button @click="isCollapsed = !isCollapsed" class="absolute -right-4 bg-bg border border-border p-1.5 rounded-full text-gray hover:scale-102 cursor-pointer shrink-0 active:text-primary">
                 <PanelLeftCloseIcon v-if="!isCollapsed" class="size-4.5" />
                 <PanelLeftOpenIcon v-if="isCollapsed" class="size-4.5" />
             </button>
         </div>
         <hr class="h-0.5 w-full border-border" />
-        <div class="space-y-3.5 px-3">
+        <div class="space-y-3.5 px-3" :class="[isCollapsed && 'w-fit']">
             <NuxtLink
                 v-for="page in pages"
                 :key="page.title"
                 :to="page.path"
-                class="flex items-center gap-2 text-gray outline outline-additional rounded-lg py-2 px-2.5 font-medium duration-200 transition-all whitespace-nowrap relative"
+                class="flex items-center gap-2 text-gray outline outline-secondary rounded-lg px-3 py-2 font-medium duration-200 transition-all whitespace-nowrap relative"
                 :class="[
-                    route.path === page.path ? 'bg-additional text-primary cursor-default' : 'hover:outline-primary',
+                    route.path === page.path ? 'bg-secondary text-light-gray cursor-default' : 'hover:outline-primary hover:text-primary',
                     isCollapsed ? 'size-12.75' : '',
                 ]"
             >
                 <component
                     :is="page.icon"
-                    class="shrink-0 duration-300 transition-all delay-100"
-                    :class="[isCollapsed ? 'size-5 mx-auto my-2' : 'size-4.5']"
+                    class="shrink-0 duration-300 transition-all"
+                    :class="[isCollapsed ? 'size-5 mx-auto my-2' : 'size-4.5', route.path === page.path && 'delay-100']"
                 />
                 <span
                     class="duration-200 transition-all origin-left"
