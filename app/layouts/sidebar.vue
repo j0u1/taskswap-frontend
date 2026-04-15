16b9c56fa21f7ab1fb1c3a22db841dcc9c6c2c60
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-15 19:02:41 +0300

    style(layout): adjust section borders and border radius

--- app/layouts/sidebar.vue
+++ app/layouts/sidebar.vue
@@ -2,7 +2,7 @@
     <div class="flex">
         <UISidebar class="hidden lg:flex duration-300 transition-all" />
         <section
-            class="p-4 lg:p-6 duration-300 transition-all w-full rounded-xl m-2 lg:m-0 mr-2.5 lg:my-2 lg:mr-2 border border-border flex flex-col gap-6.5"
+            class="p-4 lg:p-6 duration-300 transition-all w-full rounded-tl-xl m-2 lg:m-0 lg:mt-2 border-l border-t border-border flex flex-col gap-6.5"
         >
             <slot />
         </section>
